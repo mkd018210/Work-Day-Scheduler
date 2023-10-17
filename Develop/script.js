772977c1033de91eb630e389fac46b19240e430d
Mommy Me <mkm0182@yahoo.com>
2023-10-17 09:28:53 -0500

    Added more time blocks and trying to get date

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,7 @@
 // in the html.
 var saveBtn = $('#save-btn');
 saveBtn.on('click', function () {
-  
+
 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -21,6 +21,13 @@ saveBtn.on('click', function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
+  function saveReminders() {
+    localStorage.setItem("myDay", JSON.stringify(myDay));
+
+  }
   // TODO: Add code to display the current date in the header of the page.
+ function currentDay(){
+  var currentDay = moment().format('ddd, MMMM, yyyy');
+    $("#currentDay").text(currentDay);
+  }
 });
