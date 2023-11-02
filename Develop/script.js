7ff2c808ec8836437265e87f8c4a7ea5bcd9c2c1
Mommy Me <mkm0182@yahoo.com>
2023-11-01 21:14:01 -0500

    Changed intex.html & scripts

--- Develop/script.js
+++ Develop/script.js
@@ -1,62 +1,89 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+$(document).ready(function () {
+  var now = day.js().format("dddd, MMMM D YYYY");
+  var currentHour24h = day.js().hour();
+  $("#currentDay").text(now);
 
+  var storedEvents = JSON.parse(localStorage.getItem("storedEvents"));
+  if (storedEvents !== null) {
+    eventArray = storedEvents;
+  } else {
+    eventArray = new Array(8);
+    eventArray[3] = "Lunch Meeting"
+    } 
 
-const localSettings = {};
-dayjs.locale(localSettings);
-$(function () {
+    var plannerDiv = $("#planner");
+    plannerDiv.empty();
 
-  const currentHour = dayjs().format('H');
+    for (var hour = 9; hour <= 17; hour++) {
+      var newRowDiv = $("<div>");
+      newRowDiv.addClass("row time-block");
+      newRowDiv.attr("hour", hour);
 
-  function hourlyColor() {
-    $('.time-block').each(function () {
-      const blockHour = parseInt(this.id);
-      $(this).toggleClass('past', blockHour < currentHour);
-      $(this).toggleClass('present', blockHour === currentHour);
-      $(this).toggleclass('future', blockHour > currentHour);
-    });
-  }
+      var timeCol = $("<div>");
+      timeCol.addClass("col-2 col-md-1 hour text-center py-3");
 
-  function textEntry() {
-    $('.saveBtn').on('click', function () {
-      const key = $(this).parent().attr('id');
-      const value = $(this).siblings('.description').val();
-      localStorage.setItem(key, value);
-    });
-  }
-
-  function refreshColor() {
-    $('.time-block').each(function () {
-      const blockHour = parseInt(this.id);
-      if (blockHour == currentHour) {
-        $(this).removeClass('past future').addClass('present');
-      } else if (blockHour < currentHour) {
-        $(this).removeClass('future present').addClass('past');
-      } else {
-        $(this).removeClass('past present').addClass('future');
+      var amPM = "";
+      if (hour < 12 ) {
+        displayHour = hour;
+        amPM = "AM";
       }
+      if (hour > 12 ) {
+        displayHour = hour - 12;
+        amPM = "PM";
+    }
+
+    timeCol.text('${displayHour}${amPM}');
+    newRowDiv.append(timeCol);
+
+    var rowIndex = hour - 9;
+    var inputArea = $("<textara>");
+    inputArea.attr("id", 'input-${rowIndex}');
+    inputArea.attr("hour-index", rowIndex);
+    inputArea.addClass("col-8 col-md-10 description");
+    inputArea.val(eventArray[rowIndex]);
+
+    newRowDiv.append(inputArea);
+
+    var saveButtonDiv = $('<div>');
+    saveButtonDiv.attr("id", 'saveID-${rowIndex}');
+    saveButtonDiv.attr("saveID", rowIndex);
+    saveButtonDiv.addClass("btn col-2 col-md-1 saveBtn");
+
+    var saveButton = $("<i>");
+    saveButton.attr("class", "fas fa-save");
+
+    newRowDiv.append(saveButtonDiv);
+    saveButtonDiv.append(saveButton);
+
+    updateRowColor(newRowDiv, hour);
+
+    plannerDiv.append(newRowDiv);
+    }
+
+    function updateRowColor(rowHour, hour) {
+      if (hour < currentHour24h) {
+        rowHour.addClass("past");
+      } else if (hour > currentHour24h) {
+        rowHour.addClass("future");
+      } else {
+        rowHour.addClass("present");
+      }
+    };
+
+    $(".saveBtn").on('click', function (event) {
+      event.preventDefault();
+
+      var index = $(this).attr("saveID");
+      var eventInfoID = "#input-" + index;
+      var eventInfo = $(eventInfoID).val();
+
+      eventArray[index] = eventInfo;
+      localStorage.setItem("storedEvents", JSON.stringify(eventArray));
     });
-  }
-
-  $('.time-block').each(function () {
-    const key = $(this).attr('id');
-    const value = localStorage.getItem(key);
-    $(this).children('.description').val(value);
-  });
-  function updateTime() {
-    const dateElement = $('#date');
-    const timeElement = $('#time');
-    const currentDate = dayjs().format('ddd, MMMM, yyyy');
-    const currentTime = dayjs().format('hh:mm:ss');
-    dateElement.text(currentDate);
-    timeElement.text(currentTime);
-  }
-  hourlyColor();
-  textEntry();
-  refreshColor();
 });
-
 // TODO: Add code to display the current date in the header of the page.
 // TODO: Add a listener for click events on the save button. This code should
 // use the id in the containing time-block as a key to save the user input in
