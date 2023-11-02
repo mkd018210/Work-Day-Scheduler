abf9c181c2a82ee7a418e0e95b236a45b54a81ca
Mommy Me <mkm0182@yahoo.com>
2023-11-02 07:51:04 -0500

    Made changes to scripts index

--- Develop/script.js
+++ Develop/script.js
@@ -1,87 +1,29 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(document).ready(function () {
-  var now = day.js().format("dddd, MMMM D YYYY");
-  var currentHour24h = day.js().hour();
-  $("#currentDay").text(now);
+const localSettings = {};
+dayjs.local(localSettings);
 
-  var storedEvents = JSON.parse(localStorage.getItem("storedEvents"));
-  if (storedEvents !== null) {
-    eventArray = storedEvents;
-  } 
-   
+function textEntry() {
+  $('saveBtn').on('click', function () {
+    const key = $(this).parent().attr('id');
+    const value = $(this).siblings('.description').val();
+    localStorage.setItem(key, value);
+  });
+}
 
-    var plannerDiv = $("#planner");
-    plannerDiv.empty();
-
-    for (var hour = 9; hour <= 17; hour++) {
-      var newRowDiv = $("<div>");
-      newRowDiv.addClass("row time-block");
-      newRowDiv.attr("hour", hour);
-
-      var timeCol = $("<div>");
-      timeCol.addClass("col-2 col-md-1 hour text-center py-3");
-
-      var amPM = "";
-      if (hour < 12 ) {
-        displayHour = hour;
-        amPM = "AM";
-      }
-      if (hour > 12 ) {
-        displayHour = hour - 12;
-        amPM = "PM";
+function refreshColor() {
+  $('.time-block').each(function () {
+    const blockHour = parseInt(this.id);
+    if (blockHour < currentHour) {
+      blockHour.addClass("past");
+    } else if (blockHour > currentHour) {
+      blockHour.addClass("future");
+    } else {
+      blockHour.addClass("present");
     }
-
-    timeCol.text('${displayHour}${amPM}');
-    newRowDiv.append(timeCol);
-
-    var rowIndex = hour - 9;
-    var inputArea = $("<textara>");
-    inputArea.attr("id", 'input-${rowIndex}');
-    inputArea.attr("hour-index", rowIndex);
-    inputArea.addClass("col-8 col-md-10 description");
-    inputArea.val(eventArray[rowIndex]);
-
-    newRowDiv.append(inputArea);
-
-    var saveButtonDiv = $('<div>');
-    saveButtonDiv.attr("id", 'saveID-${rowIndex}');
-    saveButtonDiv.attr("saveID", rowIndex);
-    saveButtonDiv.addClass("btn col-2 col-md-1 saveBtn");
-
-    var saveButton = $("<i>");
-    saveButton.attr("class", "fas fa-save");
-
-    newRowDiv.append(saveButtonDiv);
-    saveButtonDiv.append(saveButton);
-
-    updateRowColor(newRowDiv, hour);
-
-    plannerDiv.append(newRowDiv);
-    }
-
-    function updateRowColor(rowHour, hour) {
-      if (hour < currentHour24h) {
-        rowHour.addClass("past");
-      } else if (hour > currentHour24h) {
-        rowHour.addClass("future");
-      } else {
-        rowHour.addClass("present");
-      }
-    };
-
-    $(".saveBtn").on('click', function (event) {
-      event.preventDefault();
-
-      var index = $(this).attr("saveID");
-      var eventInfoID = "#input-" + index;
-      var eventInfo = $(eventInfoID).val();
-
-      eventArray[index] = eventInfo;
-      localStorage.setItem("storedEvents", JSON.stringify(eventArray));
-    });
-});
+  })
+}
 // TODO: Add code to display the current date in the header of the page.
 // TODO: Add a listener for click events on the save button. This code should
 // use the id in the containing time-block as a key to save the user input in
